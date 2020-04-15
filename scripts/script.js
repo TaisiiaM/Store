2ba7b0036a0bdae3c2cfa9f48ee1318f9b2af52d
TaisiiaM <62401764+TaisiiaM@users.noreply.github.com>
2020-04-15 17:09:54 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 $('.my-slider').slick();
+
+const favorite = document.getElementById('favorite-products');
+const modal = document.querySelector('.modal');
+const overflow = document.createElement('div');
+
+function showModalFavorite(){
+overflow.classList.add('overflow');
+document.body.appendChild(overflow);
+const height = modal.offsetHeight;
+modal.style.marginTop = - height / 2 + 'px';
+modal.style.top = '50%'
+}
+
+overflow.addEventListener('click', () =>{ modal.style.top = '-100%'; overflow.remove()})
+favorite.addEventListener('click', showModalFavorite);
